02b8a9b8f6f2945afa355c67b2188ba1e1ff84a8
glenn <glenn@glenn-Lenovo-G480>
2016-03-04 00:25:28 +0800

    added new functions

--- wccminisite/app/browser/homepage_view.py
+++ wccminisite/app/browser/homepage_view.py
@@ -1,6 +1,7 @@
 from five import grok
 from plone.directives import dexterity, form
 from wccminisite.app.content.homepage import Ihomepage
+from Products.CMFCore.utils import getToolByName
 
 grok.templatedir('templates')
 
@@ -9,4 +10,23 @@ class Index(dexterity.DisplayForm):
     grok.require('zope2.View')
     grok.template('homepage_view')
     grok.name('view')
+    
+    @property
+    def portal_catalog(self):
+        return getToolByName(self.context, "portal_catalog")
+    
+    def contents(self):
+        context = self.context
+        
+        if context.pages_sources:
+            source_path = '/'.join(context.pages_sources.to_object.getPhysicalPath())
+            brains = self.portal_catalog.unrestrictedSearchResults(path={'query':source_path, 'depth':1},
+                                                                   portal_type='Document',
+                                                                   sort_on='created',
+                                                                   sort_order='reverse',
+                                                                   review_state='internally_published')[:5]
+            
+            return brains
+        return None
+        
 
